f2472ab6fe917bdc6914c61aaff30026194942e3
IykeMich <iykemich361@gmail.com>
2025-03-10 21:56:22 +0100

    second commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,29 +20,29 @@ export default function Header() {
       </div>
       {/* <div className='py-8 px-8 mx-8 w-full'>   */}
         <div className="nav_sector w-full h-12 z-0 absolute ">
-            <div className="nav_setup flex justify-between items-center py-8 px-8 mx-8">
+            <div className="nav_setup flex justify-between items-center py-6 xl:py-8 px-4 xl:px-8 mx-4 xl:mx-8 space-x-4 xl:space-x-8">
                 <div className="logosection flex items-center space-x-4">
                     <Image src="/images/brandLogo.svg" alt="Vercel Logo" width={30} height={50} />
-                    <h4 className='text-xl text-black font-bold'>Dhabi Restaurant</h4>
+                    <h4 className='text-base xl:text-xl text-black font-bold'>Dhabi Restaurant</h4>
                 </div>
-                <div className="Links flex items-center space-x-4">
-                    
-                    <h4 className='text-sm lg:text-base text-black font-normal'>Product</h4>
-                    <h4 className='text-sm lg:text-base text-black font-normal'>Recipes</h4>
-                    <h4 className='text-sm lg:text-base text-black font-normal'>About Us</h4>
+                <div className="Links flex items-center space-x-4">                 
+                    <h4 className='text-sm lg:text-base xl:text-lg text-black font-normal'>Product</h4>
+                    <h4 className='text-sm lg:text-base xl:text-lg text-black font-normal'>Recipes</h4>
+                    <h4 className='text-sm lg:text-base xl:text-lg text-black font-normal'>About Us</h4>
                 </div>
                 <div className="PhoneSection flex items-center space-x-4">
-                    <Button className='p-4 text-medium text-black bg-[#DAE952]
+                    <Button className='p-2 lg:p-4 text-black bg-[#DAE952]
                     hover:text-white hover:bg-black hover:shadow-md hover:shadow-gray-950
-                    transition-all duration-300 ease-in'> Special Offer </Button>
+                    transition-all duration-300 ease-in text-sm lg:text-lg font-semibold'> Special Offer </Button>
                     
-                    <h4 className='text-xl flex text-black items-center'>
-                        <PhoneCall /> <span> +2349039374531 </span>
+                    <h4 className='text-sm lg:text-base xl:text-lg flex text-black items-center'>
+                        <PhoneCall className='w-4 lg:w-8 h-4 lg:h-8'/> 
+                        <span> +2349039374531 </span>
                     </h4>
                 </div>
-                <div className="logosection flex items-center space-x-8">
-                    <ContactRoundIcon className='w-8 h-8'/>
-                    <RiShoppingBasket2Line className='w-8 h-8'/>
+                <div className="logosection flex items-center space-x-4 xl:space-x-8">
+                    <ContactRoundIcon className='w-4 lg:w-8 h-4 lg:h-8'/>
+                    <RiShoppingBasket2Line className='w-4 lg:w-8 h-4 lg:h-8'/>
                     
                 </div>
 
@@ -50,7 +50,7 @@ export default function Header() {
         
         </div>
         <div className="flex justify-between">
-            <div className="AllDel flex flex-col justify-center items-start py-8 px-8 mx-8 z-1">
+            <div className="AllDel flex flex-col justify-center items-start py-6 lg:py-8 px-4 lg:px-8 mx-4 lg:mx-8 z-1">
                 <div className="text-black leading-2 text-5xl lg:text-6xl xl:text-8xl flex flex-col mt-24">
                     <h1 className='leading-tight'>All Delicious</h1>
                     <h1 className="font-bold leading-1 tracking-wide"> Asian</h1>
@@ -62,9 +62,9 @@ export default function Header() {
                         transition-all duration-100 ease-in rounded-2xl'> View More </Button>
                 </div>
             </div>
-            <div className="z-10 absolute right-12 lg:right-24 top-20 lg:top-24 xl:top-20">
-                <Image src="/images/header/image.svg" alt="Vercel Logo" width={600} height={300} 
-                className='w-full h-[18rem] lg:h-[24rem] xl:h-[30rem]'
+            <div className="z-10 absolute right-12 lg:right-24 xl:right-16 top-20 lg:top-24 xl:top-2">
+                <Image src="/images/header/image.svg" alt="Vercel Logo" width={100} height={300} 
+                className='w-auto xl:w-96 h-[18rem] lg:h-[24rem] xl:h-[30rem]'
                 />
             </div>
         </div>
